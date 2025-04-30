88ae4c9e63ca10ecd094673bdc04f3920713fd1c
Tianrider <hadiwijayachristian7@gmail.com>
2025-04-30 14:32:25 +0700

    allow all cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const corsOptions = {
-	origin: "https://os.netlabdte.com",
+	origin: "*",
 	methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
