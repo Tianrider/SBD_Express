699edea8cf91066415127f68c3d11a523beac64f
Tianrider <hadiwijayachristian7@gmail.com>
2025-04-30 14:44:14 +0700

    allow cors local

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const corsOptions = {
-	origin: "*",
+	origin: "http://localhost:5173",
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true,
 	allowedHeaders: ["Content-Type", "Authorization"],
